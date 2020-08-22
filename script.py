6b3c7d37f55d1eacf0532aaa974b01b8fbdde358
Hakan Akile <hakanakile@hotmail.com>
2020-08-22 13:54:38 +0300

    Initial commit

---
+++ script.py
@@ -0,0 +1,19 @@
+import math
+import sys
+from os import rename
+
+import requests
+
+print( sys.version )
+print(sys.executable)
+
+
+def great(who_to_great):
+    greeting = "Hello, {}".format(who_to_great)
+    return greeting
+
+
+r = requests.get("https://coreyms.com")
+
+print(r.status_code)
+
